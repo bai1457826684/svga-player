a21beb673ba3f6d7f6efb3a9a7d6c264ab24435e
Liushuwei <1457826684@qq.com>
2024-11-09 17:41:21 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+// 自动更新应用
+require('update-electron-app')();
+
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // webPreferences: {
+    //   nodeIntegration: true
+    // }
+    webPreferences: {
+      // 预加载脚本
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
